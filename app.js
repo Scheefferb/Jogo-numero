6b0f4db8e20a4511f376fd0c396d16cf8a459a81
Scheefferb <221434185+Scheefferb@users.noreply.github.com>
2026-03-30 17:35:37 -0300

    Change number limit from 100 to 15

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 15;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela("h1", "Jogo do número secreto");
-    exibirTextoNaTela("p", "Escolha um número entre 1 e 100");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 15");
 
 }
 exibirMensagemInicial();
@@ -41,7 +41,7 @@ function gerarNumeroAleatorio() {
 let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
 let quantidadeDeElementosDaLista = listaDeNumerosSorteados.length;
 
-if (quantidadeDeElementosDaLista == 100) {
+if (quantidadeDeElementosDaLista == 15) {
     listaDeNumerosSorteados =[];
 } 
 
